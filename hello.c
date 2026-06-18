bc9f1712bffa1a33d2b3760679b317596479416e
pratham1233-cmyk <prathamankur123@gmail.com>
2026-06-18 12:46:22 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,69 @@
+//when we can't decide and whwn we are not able to compute the total number of execution of a programe we can use undoubtly or consider the total execution 
+// is nlog(n)
+//you have given an array of 6 element by taking the input from user  and solve check whether the array contains a palindrome number or not 
+//whether the array contain atleast one palindrome number or not 
+// count all the armstrong number 
+//print all the neon number form the array 
+//print all the third largest forth largest and second largest number 
+//print all the element in 2d matrix
+//convert the orientation / transpose and print the given array 6
+//count all the prime number 
+//to solve the character pattern you have to solve the following pattern
+//break the pattern into subpattern and itrate from left ot right accordingly
+// task:
+//writ a programe to performe tow sum problem 
+//WAP to print all the sub set anad all the subarrays 
+//WAP  to perform deuts=ch national plague alo=gorithm
+//WAP to find maximum sub arrays with maximum sum
+// Need to learn insertion and selection sort and merge sort
+//WAP to take two array from user find the intersection and union and element which is present in array two and but in array in one 
+//Check if there is any common in any comman element then print this sum of both the array index wise 
+//WAP to find  a subarray whose sum is equal to the target 
+// find the majority element in the array
+//you have given an array of an integer which include the element from the range 1 to n in such a way the element occur either twice or once now you have to generate an array in result which provide only those element which exist twice with main 
+//Find all the combination of the given array 
+//you have given an array whixh consist th height of the building now you have to calculate the total amount of water can be trapped within these buildings 
+//                                                        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+                                           
