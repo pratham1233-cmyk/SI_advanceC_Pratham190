9eadaff739036c616416020c9bc1a0789ebfd02d
pratham1233-cmyk <prathamankur123@gmail.com>
2026-06-23 10:41:14 +0530

    Add files via upload

---
+++ boundary_sum.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int r, c, sum = 0;
+
+    scanf("%d %d", &r, &c);
+
+    int a[r][c];
+
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            if(i == 0 || i == r - 1 || j == 0 || j == c - 1) {
+                sum += a[i][j];
+            }
+        }
+    }
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
